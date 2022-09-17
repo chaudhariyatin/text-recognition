632e45619f8311c2a1bb7e022dd43fadf065158a
chaudhariyatin <yatinchaudhari983@gmail.com>
2022-09-17 16:14:16 +0530

    native module basic setup

---
+++ android/app/src/main/java/com/ssmanager/mlkit/TextRecognitionModule.java
@@ -0,0 +1,28 @@
+//adb logcat -s TextRecognitionModule
+
+package com.ssmanager.mlkit;
+
+import android.util.Log;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+/**
+ * Created by Dmytro Portenko on 14.07.2021.
+ */
+public class TextRecognitionModule extends ReactContextBaseJavaModule {
+    TextRecognitionModule(ReactApplicationContext context) {
+        super(context);
+    }
+
+    @Override
+    public String getName() {
+        return "TextRecognitionModule";
+    }
+
+    @ReactMethod
+    public void recognizeImage(String url) {
+        Log.d("TextRecognitionModule", "Url: " + url);
+    }
+}
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,7 +1,16 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {NativeModules, StyleSheet, Text, View} from 'react-native';
+import React, {useEffect} from 'react';
+
+const {TextRecognitionModule} = NativeModules;
 
 const Home = () => {
+  useEffect(() => {
+    fetchImageDetails();
+  }, []);
+
+  const fetchImageDetails = async () => {
+    await TextRecognitionModule.recognizeImage('Hello world!');
+  };
   return (
     <View>
       <Text>Home</Text>
